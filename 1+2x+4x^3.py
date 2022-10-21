9ee121e0192f21b72437ae234147cb4a4be99be5
DionHelix <dionhelix@gmail.com>
2022-10-21 00:14:32 -0500

    Con la red de 3sin(pi*x) cambiamos la ecuación a aproximar, notamos que la red tiene un factor de -1 y está invertida con respecto a lo que queremos

--- 1+2x+4x^3.py
+++ 1+2x+4x^3.py
@@ -1,3 +1,4 @@
+from tensorflow.python import training
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
@@ -17,20 +18,11 @@ class ODEsolver(Sequential):
     
     def train_step(self, data):
         batch_size = tf.shape(data)[0]
-        x = tf.random.uniform((batch_size, 1), minval=-5, maxval=5)
-        
+        x = tf.random.uniform((batch_size, 1), minval=-1, maxval=1)
         with tf.GradientTape() as tape:
-            with tf.GradientTape() as tape2:
-                tape2.watch(x)
-                with tf.GradientTape() as tape3:
-                    tape3.watch(x)
-                    y_pred = self(x, training=True)    
-                dy = tape3.gradient(y_pred, x)
-            dy2 = tape2.gradient(dy, x)    
-            x_0 = tf.zeros((batch_size), 1)
-            y_0 = self(x_0, training=True)
-            eq = 1+2*y_pred+4*tf.math.pow(y_pred, 3)
-            loss = keras.losses.mean_squared_error(0., eq)
+            y_pred = self(x, training=True)
+            eq = y_pred - 1+2*x+4*tf.math.pow(x, 3)
+            loss = keras.losses.mean_squared_error(0, eq) 
         grads =  tape.gradient(loss, self.trainable_variables)
         self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
         self.loss_tracker.update_state(loss)
@@ -38,8 +30,7 @@ class ODEsolver(Sequential):
 
 model = ODEsolver()
 
-model.add(Dense(10, activation='sigmoid', input_shape=(1,)))
-model.add(Dense(10, activation='tanh'))
+model.add(Dense(10, activation='tanh', input_shape=(1,)))
 model.add(Dense(10, activation='tanh'))
 model.add(Dense(10, activation='tanh'))
 model.add(Dense(10, activation='tanh'))
@@ -47,12 +38,12 @@ model.add(Dense(1, activation='linear'))
 
 model.summary()
 
-model.compile(optimizer=Adam(), metrics=['loss'])
+model.compile(optimizer=RMSprop(), metrics=['loss'])
 x=tf.linspace(-1, 1, 100)
 history = model.fit(x, epochs=500, verbose = 1)
 
 x_testv = tf.linspace(-1, 1, 100)
 a=model.predict(x_testv)
 plt.plot(x_testv, a)
-plt.plot(x_testv, -0.385)
+plt.plot(x_testv, 1+2*x+4*np.power(x, 3))
 plt.show()
